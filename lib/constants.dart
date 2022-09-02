d2a0433a02e669a74c832f3a96ec31012f38c1ff
Pedro Leite <pedrocaleite@gmail.com>
2022-09-02 10:57:10 -0700

    version 2

--- lib/constants.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-const primaryColor = Color.fromARGB(255, 230, 223, 223);
-const secondaryColor = Color.fromARGB(255, 255, 255, 255);
-const bgColor = Color.fromARGB(255, 241, 241, 241);
-
-const defaultPadding = 16.0;
-const profilePicPadding = 20.0;
-const profilePicPaddingRight = 40.0;
